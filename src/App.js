d1d50fb18795aee81b53a049fbeff808c78461b3
Charif Abdeslam <charifabdeslem@gmail.com>
2018-02-21 00:24:27 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data : []
+    }
+  }
+  componentDidMount(){
+    axios.get('https://api.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist')
+         .then(response=> {
+           this.setState({data:response.data})
+       }).then(()=>{
+         console.log(this.state.data)
+       })
+
+  }
   render() {
     return (
       <div className="container text-center">
-    <h1>HELLO WORLD</h1>
       </div>
     );
   }
